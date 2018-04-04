c30d4fbee2761684f3b897699585537eb8605266
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-03 23:15:53 -0300

    Devmind commit in 2018-04-03 23:15:52

--- formulario.php
+++ formulario.php
@@ -19,27 +19,26 @@ Numero1 :<input type="number"
 
 		
 	<?php
-	
-		
+
+echo "hhehehehehehehehehe";
+        
 // $_POST é uma array, contendo todos os parâmetros que são enviados pelo método post, e eu só quero mostrar alguma coisa na tela caso essa array não esteja vazia
 // ou seja, quando o usuário mandou alguma coisa =p
 // !empty signigica NÃO vazio. No php, o operador ! significa uma negação.
 
-if(!empty($_POST)){
+if (!empty($_POST)) {
 
 
 // foreach é uma estrutura de repetição. Este trecho de código significa: pra cada chave/valor ( $key=>$value) dentro da array $_POST, eu quero verificar se o valor é vazio,
-// e se for, eu quero deletar a posição dele dentro da array $_POST, o removendo por consequência.
-foreach($_POST as $key=>$value){
-if($value == ""){
+    // e se for, eu quero deletar a posição dele dentro da array $_POST, o removendo por consequência.
+    foreach ($_POST as $key=>$value) {
+        if ($value == "") {
+            unset($_POST[$key]);
+        }
+    }
 
-unset($_POST[$key]);
-
-}
-}
-
-// Depois de feitos os tratamentos, eu mostro os maiores e menores valores restantes dentro da array $_POST
-echo "O maior valor e: " . max($_POST) . ", e o menor e: " . min($_POST); 
+    // Depois de feitos os tratamentos, eu mostro os maiores e menores valores restantes dentro da array $_POST
+    echo "O maior valor e: " . max($_POST) . ", e o menor e: " . min($_POST);
 }
     
 
