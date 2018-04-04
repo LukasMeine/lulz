6bb5e2bae074fd06e761d6b283ee6ac8e8dbd732
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-03 23:05:54 -0300

    Devmind commit in 2018-04-03 23:05:53

--- formulario.php
+++ formulario.php
@@ -20,8 +20,6 @@ Numero1 :<input type="number"
 		
 	<?php
 	
-	echo "ilha pig";
-	
 		
 // $_POST é uma array, contendo todos os parâmetros que são enviados pelo método post, e eu só quero mostrar alguma coisa na tela caso essa array não esteja vazia
 // ou seja, quando o usuário mandou alguma coisa =p
