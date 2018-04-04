f13eb695e6f7b8edb067695160cad4433bf4667d
Lucas Meine <joaolucas.meine@redes.unb.br>
2018-04-04 00:36:38 -0300

    Devmind commit in 2018-04-04 00:36:37

--- formulario.php
+++ formulario.php
@@ -20,7 +20,7 @@ Numero1 :<input type="number"
 		
 	<?php
         
-        //é isso  asjhdkjhaskjhdak hehehehe
+        //é isso  asjhdkjhaskjhdak uahuhauhauha XD
         
         
 // $_POST é uma array, contendo todos os parâmetros que são enviados pelo método post, e eu só quero mostrar alguma coisa na tela caso essa array não esteja vazia
